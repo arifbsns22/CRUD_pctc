48921a7978c8c1e21e083b5b0badefeb7839ad8f
mohsin <arifbsns22@gmail.com>
2025-05-06 12:43:10 +0600

    Flutter CRUD with RestAPI

---
+++ lib/Screen/ProductCreatScreen.dart
@@ -0,0 +1,129 @@
+import 'package:crud/Style/style.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProductCreateScreen extends StatefulWidget {
+  @override
+  State<ProductCreateScreen> createState() => _ProductCreateScreenState();
+}
+
+class _ProductCreateScreenState extends State<ProductCreateScreen> {
+
+//---------Product info collection model as like RestAPI----//
+  Map<String,String> FormValues={"ProductName": "", "ProductCode": "", "Img": "", "Qty": "", "UnitPrice": "", "TotalPrice": ""};
+  InputOnChange(MapKey, Textvalue){
+    setState(() {
+      FormValues.update(MapKey, (value)=> Textvalue);
+    });
+
+  }
+//---------Product info collection model as like RestAPI----//
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create Product"),
+      ),
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            child: (SingleChildScrollView(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+
+
+                  TextFormField(
+                    onChanged: (Textvalue) {
+                      InputOnChange("ProductName",Textvalue);
+                    },
+                    decoration: AppInputDecoration('Product Name'),
+                  ),
+
+
+                  SizedBox(height: 20),
+                  TextFormField(
+                    onChanged: (Textvalue) {
+                      InputOnChange("ProductCode",Textvalue);
+                    },
+                    decoration: AppInputDecoration('Product Code'),
+                  ),
+
+
+                  SizedBox(height: 20),
+                  TextFormField(
+                    onChanged: (Textvalue) {
+                      InputOnChange("Img",Textvalue);
+                    },
+                    decoration: AppInputDecoration('Product Image'),
+                  ),
+
+
+                  SizedBox(height: 20),
+                  TextFormField(
+                    onChanged: (Textvalue) {
+                      InputOnChange("UnitPrice",Textvalue);
+                    },
+                    decoration: AppInputDecoration('Unit Price'),
+                  ),
+
+
+                  SizedBox(height: 20),
+                  TextFormField(
+                    onChanged: (Textvalue) {
+                      InputOnChange("TotalPrice",Textvalue);
+                    },
+                    decoration: AppInputDecoration('Total Price'),
+                  ),
+
+
+                  SizedBox(height: 20),
+                  AppDropDownStyle(DropdownButton(
+                    value: FormValues ['Qty'],
+                    items: [
+                      DropdownMenuItem(
+                        child: Text('Select Quantity'),
+                        value: "",
+                      ),
+                      DropdownMenuItem(
+                        child: Text('1 pcs'),
+                        value: "1 pcs",
+                      ),
+                      DropdownMenuItem(
+                        child: Text('2 pcs'),
+                        value: "2 pcs",
+                      ),
+                      DropdownMenuItem(
+                        child: Text('3 pcs'),
+                        value: "3 pcs",
+                      ),
+                      DropdownMenuItem(
+                        child: Text('4 pcs'),
+                        value: "4 pcs",
+                      ),
+                    ],
+                    onChanged: (Textvalue) {
+                      InputOnChange("Qty",Textvalue);
+
+                    },
+                    underline: Container(),
+                    isExpanded: true,
+                  )),
+                  SizedBox(height: 20),
+                  Container(
+                      child: ElevatedButton(
+                          style: AppButtonStyle(),
+                          onPressed: () {},
+                          child: SuccessButtonChild('Submit')))
+                ],
+              ),
+            )),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Style/style.dart
@@ -0,0 +1,69 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+const colorRed = Color.fromRGBO(231, 28, 36, 1);
+const colorGreen = Color.fromRGBO(33, 191, 115, 1);
+const colorWhite = Color.fromRGBO(255, 255, 255, 1);
+const colorDarkBlue = Color.fromRGBO(44, 62, 80, 1);
+
+SvgPicture ScreenBackground(context) {
+  return SvgPicture.asset(
+      'assets/images/143489742_2cf671e6-48f4-4409-b2ea-055880bbeb90.svg.svg',
+      alignment: Alignment.bottomCenter,
+      width: MediaQuery.of(context).size.width,
+      height: 40);
+}
+
+DecoratedBox AppDropDownStyle(child) {
+  return DecoratedBox(
+      decoration: BoxDecoration(
+        color: colorWhite,
+        border: Border.all(color: colorWhite, width: 1),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+        child: child,
+      ));
+}
+
+ButtonStyle AppButtonStyle() {
+  return ElevatedButton.styleFrom(
+      elevation: 1,
+      padding: EdgeInsets.all(0),
+      backgroundColor: Colors.transparent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)));
+}
+
+Ink SuccessButtonChild(ButtonText) {
+  return Ink(
+    decoration: BoxDecoration(
+      color: colorGreen,
+      borderRadius: BorderRadius.circular(6),
+    ),
+    child: Container(
+      height: 45,
+      alignment: Alignment.center,
+      child: Text(
+        ButtonText,
+        style: TextStyle(
+            fontWeight: FontWeight.w400, fontSize: 15, color: colorWhite),
+      ),
+    ),
+  );
+}
+
+InputDecoration AppInputDecoration(label) {
+  return InputDecoration(
+      focusedBorder: const OutlineInputBorder(
+          borderSide: const BorderSide(color: colorGreen, width: 1)),
+      fillColor: colorWhite,
+      filled: true,
+      contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
+      enabledBorder: const OutlineInputBorder(
+          borderSide: const BorderSide(color: colorWhite, width: 0)),
+      border: OutlineInputBorder(),
+      labelText: label);
+}
